289f2edcdb9e5e54287f3a12e97ecd64f3e27cfa
Amro <2592997@students.wits.ac.za>
2025-02-02 17:03:07 +0200

    Update

--- Javascript/volunteerPage.js
+++ Javascript/volunteerPage.js
@@ -1,3 +1,4 @@
 document.getElementById("submit-button").addEventListener("click", function(event) {
+    console.log("the volunteer button was clicked");
     event.preventDefault(); 
 });
--- Volunteer/Includes/volunteer_view.inc.php
+++ Volunteer/Includes/volunteer_view.inc.php
@@ -5,6 +5,7 @@ declare(strict_types= 1);
 function check_signup_errors(){
     if (isset($_SESSION["errors_signup"])){
         $errors = $_SESSION["errors_signup"];
+        echo '<script>console.log("It is checking for errors");</script>';
 
         echo '<div id="error-message">';
 
