6fb77fdcbbe8a5efd202d9d49df930a806efe018
Amro <2592997@students.wits.ac.za>
2025-02-09 15:24:58 +0200

    IDK

--- Volunteer/Includes/volunteer.inc.php
+++ Volunteer/Includes/volunteer.inc.php
@@ -1,9 +1,8 @@
 <?php
 
-var_dump("sdfsdf");
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_errors.log'); // Logs errors in a file
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
